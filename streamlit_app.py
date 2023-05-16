60f46c5a9734699bb02296c679f797fc49e65eb8
dongkyu20 <79791066+dongkyu20@users.noreply.github.com>
2023-05-16 12:35:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
-
+st.tilte("this is the app title")
+st.header("this is the mark")
+st.markdowni("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the cpation")
+st.code("x=2021")
+st.latex(r'"a+a r^1 +a r^2+a r^3"')
